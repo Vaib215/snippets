713ca3905d85d5de7a8ca86e1d0be725bbfd1e6c
Restyled.io <commits@restyled.io>
2024-07-15 13:05:20 +0000

    Restyled by pg_format

--- db/drizzle/0001_flat_ravenous.sql
+++ db/drizzle/0001_flat_ravenous.sql
@@ -1,2 +1,6 @@
-ALTER TABLE "snippet" ALTER COLUMN "user_id" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "snippet" ALTER COLUMN "user_id" DROP NOT NULL;
+ALTER TABLE "snippet"
+    ALTER COLUMN "user_id" SET DATA TYPE text;
+
+--> statement-breakpoint ALTER TABLE "snippet"
+    ALTER COLUMN "user_id" DROP NOT NULL;
+
